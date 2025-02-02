bf5b506cb79f4d26a6576feb1ddb088bb1feab93
FDIL <fdil.fadilh1@gmail.com>
2025-02-01 21:47:19 -0700

    To script, added creating stow symlinks
    This allows for install.sh to make sure the dotfiles are actually used by the system,
    by creating the nessacary symlinks.

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
  #!/bin/bash
 
+# Putting dotfiles into correct place using GNU stow
+stow --adpot *  # first create all symlinks, use adopt to not deal with issues of existing files
+git reset --hard origin/main
+# What happened here is when we used adopt, any files that already existed in our system would overwrite the ones in dotfiles.
+# Then to make sure we got the correct versions of all our files, we reset to match what is on the Github repo
+
 # Manually installed tmux plugins
 mkdir -p ~/.config/tmux/plugins/catppuccin
 mkdir -p ~/.config/tmux/plugins/tpm
@@ -9,4 +15,4 @@ git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
 git clone -b v2.1.2 https://github.com/catppuccin/tmux.git ~/.config/tmux/plugins/catppuccin/tmux
 
 
-
+# Go to https://github.com/catppuccin/grub/tree/main for instructions on setting catppuccin theme for grub
