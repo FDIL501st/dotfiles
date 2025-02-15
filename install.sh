ff8a04cfe6112af77dac043923addabff0d68e7d
FDIL <fdil.fadilh1@gmail.com>
2025-02-14 19:56:02 -0700

    Update the stow --adopt command with better syntax

--- install.sh
+++ install.sh
@@ -9,13 +9,11 @@ fi
 
 
 # Putting dotfiles into correct place using GNU stow
-stow --adopt *  # first create all symlinks, use adopt to not deal with issues of existing files
+stow --adopt .  # first create all symlinks, use adopt to not deal with issues of existing files
 git reset --hard HEAD
 # What happened here is when we used adopt, any files that already existed in our system would overwrite the ones in dotfiles.
 # Then to make sure we got the correct versions of all our files, we reset to match what is on the Github repo
 
-# actually run stow
-stow .
 
 # Before using git clone, need to make sure directories being cloned to don't exist
 TPM_DIR="$HOME/.config/tmux/plugins/tpm"
